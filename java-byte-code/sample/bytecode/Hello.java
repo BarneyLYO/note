d10072b9a207c16dd30b0cb81fe4b2834bdc9e23
BarneyLYO <barneyliu@hotmail.com>
2022-11-01 16:30:27 +0800

    java related

---
+++ java-byte-code/sample/bytecode/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+  private void a () {
+      System.out.println(123456l);
+  }
+
+  public String toString() {
+    return super.toString();
+  }
+
+  public static void main (String[] args) {
+    new Hello();
+    System.out.println("Hello World");
+  }
+}
